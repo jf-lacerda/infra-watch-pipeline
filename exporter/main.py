e98a1ccd9c265ad411c5c350950f639c6a24eb2e
Joao Saldanho C. de Lacerda Filho <jlacerda@viper.local>
2025-05-05 16:07:06 -0300

    Primeiro commit do projeto infra-watch-pipeline

---
+++ exporter/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Response
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
+import speedtest
+import threading
+
+app = FastAPI()
+
+download_gauge = Gauge("internet_download_speed_mbps", "Download speed in Mbps")
+upload_gauge = Gauge("internet_upload_speed_mbps", "Upload speed in Mbps")
+ping_gauge = Gauge("internet_ping_ms", "Ping in ms")
+
+def run_speedtest():
+    try:
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        download = st.download() / 1_000_000
+        upload = st.upload() / 1_000_000
+        ping = st.results.ping
+
+        download_gauge.set(round(download, 2))
+        upload_gauge.set(round(upload, 2))
+        ping_gauge.set(round(ping, 2))
+    except Exception as e:
+        print(f"Erro no speedtest: {e}")
+
+@app.get("/")
+def index():
+    threading.Thread(target=run_speedtest).start()
+    return {"message": "Speedtest iniciado!"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
